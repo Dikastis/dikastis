ce29abbe84b2306f11708dd4cdab29f332f4b85c
Piyush <piyush.srivastava.63@gmail.com>
2016-05-08 13:17:00 +0530

    Server Add problems window raw

--- server_modules/server_window_handler.py
+++ server_modules/server_window_handler.py
@@ -4,6 +4,8 @@ import thread
 from server_modules.client_modules_for_server import start_judge
 from server_modules.start_server_module import startServer
 import tkMessageBox
+import ttk
+# from server_modules.database import *
 
 try:
     # Python2
@@ -17,52 +19,78 @@ q = []
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 class MainWindow(tk.Frame):
-    
+  
+
     def __init__(self, *args, **kwargs):
         self.s = s
+        
+
+        def problem_window(self):
+            def focus_next_window(event):
+                event.widget.tk_focusNext().focus()
+                return("break")
+
+            labelText = tk.StringVar()
+            labelText.set('Enter Problem Name')
+            label1 = tk.Label(self, textvariable=labelText, height=1)
+            label1.pack()
+
+            problem_name = tk.Text(self, height=1, width=30)
+            problem_name.pack()
+            problem_name.insert(tk.END, "")
+            
+            problem_name.bind("<Tab>", focus_next_window)
+
+            labelText = tk.StringVar()
+            labelText.set('Enter Problem Code')
+            label1 = tk.Label(self, textvariable=labelText, height=1)
+            label1.pack()
+
+            problem_code = tk.Text(self, height=1, width=30)
+            problem_code.pack()
+            problem_code.insert(tk.END, "")
+
+            problem_code.bind("<Tab>", focus_next_window)
+
+            button = tk.Button(self, text="Add", command=lambda: addProblem(problem_name , problem_code , listbox))
+            button.pack()
+
+            listbox = tk.Listbox(self,width=30)
+            listbox.pack()
+            listbox.insert(tk.END, "problems:")
+
+
+
         tk.Frame.__init__(self, *args, **kwargs)
-        self.button = tk.Button(self, text="Add Problems for Contest", 
-                                command=self.create_window)
-        self.button.pack()
-
-        self.button = tk.Button(self, text="Start Server", 
-                                command=lambda: startServer(total_problems , q , s))
-        self.button.pack()
-
-    def create_window(self):
-        problem_add_window = tk.Toplevel(self)
-        problem_add_window.wm_title('Add problems For Contest')
-        labelText = tk.StringVar()
-        labelText.set('Enter Problem Name')
-        label1 = tk.Label(problem_add_window, textvariable=labelText, height=1)
-        label1.pack()
-
-        problem_name = tk.Text(problem_add_window, height=1, width=30)
-        problem_name.pack()
-        problem_name.insert(tk.END, "")
-
-        labelText = tk.StringVar()
-        labelText.set('Enter Problem Code')
-        label1 = tk.Label(problem_add_window, textvariable=labelText, height=1)
-        label1.pack()
-
-        problem_code = tk.Text(problem_add_window, height=1, width=30)
-        problem_code.pack()
-        problem_code.insert(tk.END, "")
-
-        button = tk.Button(problem_add_window, text="Add", command=lambda: addProblem(problem_name , problem_code , problem_add_window))
-        button.pack()
+        tk.Frame(self,width=600,height=400)
+        
+        notebook = ttk.Notebook(self)
+        notebook.pack()
+        subframe = tk.Frame(self,width=280,height=400)
+        subframe.pack()
+        notebook.add(subframe, text="ADD Problems", state="normal")
+        problem_window(subframe)
+        
+        subframe1 = tk.Frame(self,width=280,height=400)
+        subframe1.pack()
+        notebook.add(subframe1, text="All Problem List", state="normal")
+        
+        subframe21 = tk.Frame(self,width=280,height=400)
+        subframe21.pack()
+        notebook.add(subframe21, text="Broadcast", state="normal")
 
 
+def addProblem(problem_name , problem_code, listbox):
+        global total_problems
+        global q
 
-def addProblem(problem_name , problem_code , problem_add_window):
-    global total_problems
-    global q
-    
-    total_problems = total_problems + 1
-    
-    print problem_name.get("1.0",tk.END) + '$%$' + problem_code.get("1.0",tk.END)
-    q.append(str(total_problems) + '.)' + problem_name.get("1.0",tk.END) + '$%$' + problem_code.get("1.0",tk.END))
-    tkMessageBox.showinfo('Info', 'Problem Successfully Added To Contest!!')
-    problem_add_window.destroy
-    
+        total_problems = total_problems + 1
+        item = 'name: ' + problem_name.get("1.0",tk.END) + ' code: ' + problem_code.get("1.0",tk.END)
+        listbox.insert(tk.END,item)
+        print problem_name.get("1.0",tk.END) + '$%$' + problem_code.get("1.0",tk.END)
+        q.append(str(total_problems) + '.)' + problem_name.get("1.0",tk.END) + '$%$' + problem_code.get("1.0",tk.END))
+        tkMessageBox.showinfo('Info', 'Problem Successfully Added To Contest!!')
+        problem_name.delete('1.0', tk.END)
+        problem_code.delete('1.0', tk.END)
+        
+
